466d21de9279e73820b47d1b4abe93d7e7f6d950
keri19372 <keri19372@gmail.com>
2017-03-04 13:26:01 +0200

    Finish homework №9

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
 var ball = document.querySelector(".discoball");
 
+	var classes = ["here", "goes", "our", "awesome", "disco", "ball"];
+	var i = 0;
+
+
 function goDisco() {
-	//do your disco magic here !
+	//console.log(classes[i]);
+	ball.setAttribute("class","discoball " + classes[i]);
+	i++;
+	if(i > 5) {
+		i=0;
+	}
+
 }
 
 setInterval(goDisco,300);
